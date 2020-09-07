47180cb2ef62a61ba662777b82739fdce41cf2c3
greencoder001 <64889695+greencoder001@users.noreply.github.com>
2020-09-07 17:47:07 +0200

    🔥 PWA
    Nun ist der Noten Planer auch als (Progressive) Web-App auf dem Handy verfügbar!

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-/* global $$, $ */
+/* global $$, $, np */
 
 document.write(`<meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -26,6 +26,14 @@ var navigation = {
       }
     },
     {
+      name: 'Fächer',
+      href: 'subjects',
+      icon: {
+        type: 's',
+        name: 'boxes'
+      }
+    },
+    {
       name: 'Deine Klasse',
       href: 'classroom',
       icon: {
@@ -35,8 +43,8 @@ var navigation = {
     }
   ]
 }
-navigation.items.forEach((item) => {
-  nav += `<div class="nav-item" onclick="${item.href}" title="${item.name}"><i class="fa${item.icon.type} fa-${item.icon.name}"></i></div>`
+navigation.items.forEach((item, i) => {
+  nav += `<div data-nav-index="${i}" class="nav-item fa${item.icon.type} fa-${item.icon.name}" onclick="np.loadContent(this.getAttribute('data-nav-index'))" title="${item.name}"></div>`
 })
 
 // ####################################
@@ -45,10 +53,11 @@ $$(document)(() => {
   $.loadStyle(`
     /* Colors: */
     :root {
-      --main: #f23434;
+      --main: #910c0c;
       --bg: #fff;
       --text: #000;
       --mtxt: #fff;
+      --a-color: #c4c2c2;
     }
   `)
 
@@ -57,4 +66,31 @@ $$(document)(() => {
     <main></main>
     <nav>${nav}</nav>
   `
+
+  // ####################################
+
+  window.np = {
+    currentPage: $.storage.local.get('np-currentPage') || 0,
+    main: $$('body main'),
+    loadContent: function (navId) {
+      np.currentPage = navId
+
+      $$('body nav div.nav-item').removeClass('active')
+      $$(`body nav div.nav-item[data-nav-index="${navId}"]`).addClass('active')
+
+      $.storage.local.set('np-currentPage', navId)
+
+      np.loadContentInToMain()
+    },
+    loadContentInToMain: function () {
+      const cp = navigation.items[np.currentPage]
+      np.main.innerHTML = `
+        <h1>${cp.name}</h1>
+      `
+    }
+  }
+
+  // ####################################
+
+  np.loadContent(np.currentPage)
 })
